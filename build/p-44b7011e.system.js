756645d1b1d666980614aaf83c7b8ebaa916ba37
Steve Zelek <szelek@ionic.io>
2022-05-29 13:47:47 -0700

    publish gh page

---
+++ build/p-44b7011e.system.js
@@ -0,0 +1,1 @@
+System.register(["./p-84581235.system.js"],(function(){"use strict";var t,e;return{setters:[function(n){t=n.p;e=n.b}],execute:function(){t().then((function(t){return e([["p-2d067b93.system",[[1,"my-component"]]]],t)}))}}}));
